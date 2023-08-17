27d87bfc6b69ebbb90e3b7f918ca3ef90b9cdc67
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-17 14:37:47 +0200

    Fixed orders and handled some errors

--- api/index.js
+++ api/index.js
@@ -2,10 +2,27 @@ const { express, routes } = require("./controller");
 const path = require("path");
 const port = +process.env.PORT || 3000;
 const app = express();
+const cors = require("cors");
+const { errorHandling } = require("./middleware/ErrorHandling");
+const cookieParser = require("cookie-parser");
 
-app.use(express.static("./static"));
 
-app.use(express.urlencoded({ extended: false }), routes);
+app.use(express.static("./static"),
+  express.urlencoded({ extended: false }),
+  cookieParser(),
+  cors(),
+  routes
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
 
 routes.get("^/$|/challenger", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./static/html/index.html"));
